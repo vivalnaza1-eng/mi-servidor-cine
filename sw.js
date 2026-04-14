181bd2ba7e4ebbf1e543876beb3de740a60c505c
vivalnaza1-eng <vivalnaza1@gmail.com>
2026-04-14 09:08:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+// Este es el Service Worker
+self.addEventListener('install', (event) => {
+  console.log('SW instalado');
+});
 
-"start_url": "./",
-"scope": "./",
+self.addEventListener('fetch', (event) => {
+  // Esto permite que la app cargue contenido online sin errores en iOS
+});
